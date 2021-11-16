14a5f982a986e3e3adbe3d1ca55c85f806da956f
shchoi94 <hyuklassic@gmail.com>
2021-11-16 12:06:27 +0900

    [main] Fix: model ResearchInformation
    - blank=True 옵션을 추가했습니다.

--- research/models.py
+++ research/models.py
@@ -1,18 +1,18 @@
-from django.db   import models
+from django.db import models
 
-class ResearchInformation(models.Model): 
 
-    class Meta: 
-        db_table = 'research_informations'
+class ResearchInformation(models.Model):
+    class Meta:
+        db_table = 'research_information'
 
-    name          = models.CharField(max_length=100)
-    number        = models.CharField(max_length=10, unique=True)
-    period        = models.CharField(max_length=10, blank=True)
-    range         = models.CharField(max_length=20)
-    code          = models.CharField(max_length=20)
-    institute     = models.CharField(max_length=20)
-    stage         = models.CharField(max_length=20)
-    target_number = models.IntegerField()
-    office        = models.CharField(max_length=20)
-    created_at    = models.DateField(auto_now_add=True)
-    updated_at    = models.DateField(auto_now=True)
+    name = models.CharField(max_length=100, blank=True)
+    number = models.CharField(max_length=10, unique=True)
+    period = models.CharField(max_length=10, blank=True)
+    range = models.CharField(max_length=20, blank=True)
+    code = models.CharField(max_length=20, blank=True)
+    institute = models.CharField(max_length=20, blank=True)
+    stage = models.CharField(max_length=20, blank=True)
+    target_number = models.IntegerField(blank=True)
+    office = models.CharField(max_length=20, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
