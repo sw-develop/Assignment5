7f3567a7ab101d5df1cc53dadcc612767c735a0f
Jiwon <yesjiwon5304@gmail.com>
2021-11-16 21:11:30 +0900

    Add: 배포용 도커 파일

--- research/urls.py
+++ research/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from research.views import ResearchRetrieveView, SearchResearchView, RecentListView
 
 urlpatterns = [
+    path('/recent', RecentListView.as_view(), name='recent'),
     path('/<str:number>', ResearchRetrieveView.as_view(), name='detail'),
     path('', SearchResearchView.as_view(), name='search-research'),
-    path('/recent', RecentListView.as_view(), name='recent'),
 ]
