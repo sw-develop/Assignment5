0b24e05479b0501cbd0f816bc1d45ac2f0a138f1
shchoi94 <hyuklassic@gmail.com>
2021-11-16 14:29:44 +0900

    [feature#9] Refactor: _change_key method

--- research/utils.py
+++ research/utils.py
@@ -18,27 +18,18 @@ def convert_research_data_to_model_form(data_list: List[dict]) -> List[dict]:
 
 
 def _change_key(old_key):
-    if old_key == '과제명':
-        new_key = 'name'
-    elif old_key == '과제번호':
-        new_key = 'number'
-    elif old_key == '연구기간':
-        new_key = 'period'
-    elif old_key == '연구범위':
-        new_key = 'range'
-    elif old_key == '연구종류':
-        new_key = 'code'
-    elif old_key == '연구책임기관':
-        new_key = 'institute'
-    elif old_key == '임상시험단계(연구모형)':
-        new_key = 'stage'
-    elif old_key == '전체목표연구대상자수':
-        new_key = 'target_number'
-    elif old_key == '진료과':
-        new_key = 'office'
-    else:
-        print(old_key)
-    return new_key
+    keys = {
+        "과제명": "name",
+        "과제번호": "number",
+        "연구기간": "period",
+        "연구범위": "range",
+        "연구종류": "code",
+        "연구책임기관": "institute",
+        "임상시험단계(연구모형)": "stage",
+        "전체목표연구대상자수": "target_number",
+        "진료과": "office"
+    }
+    return keys[old_key]
 
 
 def convert_dict_to_dataclass(dcls, before, many=False):
