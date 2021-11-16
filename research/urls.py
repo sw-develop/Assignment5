e0eb0dbf60b176d4f8bd4709b7bb8b37ea207855
sw-develop <psw00719@naver.com>
2021-11-16 16:35:48 +0900

    [feature#6] Add: Search Research View - 기본 Filtering 구현 (적용 필드 : name, range, code, institue, stage, target_number, office) - Search Filter 적용 (적용 필드 : name, number, range, code, institute, stage, office) - Order Filter 적용 (기준 : updated_at 내림차순)

--- research/urls.py
+++ research/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from research.views import ResearchRetrieveView
+from research.views import ResearchRetrieveView, SearchResearchView
 
 urlpatterns = [
     path('/<str:number>', ResearchRetrieveView.as_view(), name='detail'),
+    path('', SearchResearchView.as_view(), name='search-research'),
 ]
--- research/views.py
+++ research/views.py
@@ -1,10 +1,22 @@
-from rest_framework.generics import RetrieveAPIView
+from django_filters.rest_framework  import DjangoFilterBackend
+from rest_framework.filters         import OrderingFilter, SearchFilter
+from rest_framework.generics        import RetrieveAPIView, ListAPIView
 
-from research.models import ResearchInformation
-from .serializers    import ResearchInformationSerializer
+from research.models    import ResearchInformation
+from .filters           import ResearchFilter
+from .serializers       import ResearchInformationSerializer
 
 
 class ResearchRetrieveView(RetrieveAPIView):
-    queryset = ResearchInformation.objects.all()
-    serializer_class = ResearchInformationSerializer
-    lookup_field = 'number'
+    queryset            = ResearchInformation.objects.all()
+    serializer_class    = ResearchInformationSerializer
+    lookup_field        = 'number'
+
+
+class SearchResearchView(ListAPIView):
+    queryset            = ResearchInformation.objects.all()
+    serializer_class    = ResearchInformationSerializer
+    filter_backends     = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_class     = ResearchFilter
+    ordering            = ['-updated_at']
+    search_fields       = ['name', 'number', 'range', 'code', 'institute', 'stage', 'office']
