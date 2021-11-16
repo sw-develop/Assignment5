a7f8947672f73b2c75cf28e5bbe4c1e34a0d5e80
sw-develop <psw00719@naver.com>
2021-11-16 16:35:48 +0900

    [feature#6] Add: Search Research View - 기본 Filtering 구현 (적용 필드 : name, range, code, institue, stage, target_number, office) - Search Filter 적용 (적용 필드 : name, number, range, code, institute, stage, office) - Order Filter 적용 (기준 : updated_at 내림차순)

---
+++ research/filters.py
@@ -0,0 +1,18 @@
+from django_filters     import rest_framework as filters
+
+from research.models    import ResearchInformation
+
+
+class ResearchFilter(filters.FilterSet):
+    name                = filters.CharFilter(field_name="name", lookup_expr="icontains")
+    range               = filters.CharFilter(field_name="range", lookup_expr="icontains")
+    code                = filters.CharFilter(field_name="code", lookup_expr="icontains")
+    institute           = filters.CharFilter(field_name="code", lookup_expr="icontains")
+    stage               = filters.CharFilter(field_name="stage", lookup_expr="icontains")
+    min_target_number   = filters.NumberFilter(field_name="target_number", lookup_expr="gte")
+    max_target_number   = filters.NumberFilter(field_name="target_number", lookup_expr="lte")
+    office              = filters.CharFilter(field_name="office", lookup_expr="icontains")
+
+    class Meta:
+        model = ResearchInformation
+        exclude = ('number', 'period', 'created_at', 'updated_at')
